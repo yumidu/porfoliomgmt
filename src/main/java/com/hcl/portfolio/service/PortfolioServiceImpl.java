e61e073bde4a259a1c1234be523ecbc181db7a50
iroshax <irosha.senevirathna@gmail.com>
2024-04-27 13:04:12 +0800

    Added package structure and tests

---
+++ src/main/java/com/hcl/portfolio/service/PortfolioServiceImpl.java
@@ -0,0 +1,12 @@
+package com.hcl.portfolio.service;
+
+import com.hcl.portfolio.model.Portfolio;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PortfolioServiceImpl implements PortfolioService{
+    @Override
+    public Portfolio getPortfolio(int i) {
+        return null;
+    }
+}
--- src/test/java/com/hcl/portfolio/PortfolioApplicationTests.java
+++ src/test/java/com/hcl/portfolio/PortfolioApplicationTests.java
@@ -1,13 +1,39 @@
 package com.hcl.portfolio;
 
+import com.hcl.portfolio.model.Portfolio;
+import com.hcl.portfolio.repository.PortfolioRepository;
+import com.hcl.portfolio.service.PortfolioService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import java.math.BigDecimal;
+import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class PortfolioApplicationTests {
 
+
+
+	@Autowired
+	private PortfolioService portfolioService;
+
+	@MockBean
+	private PortfolioRepository portfolioRepository;
+
 	@Test
-	void contextLoads() {
+	public void shouldReturnPortfolioData() {
+
+		BigDecimal portfolioValue = new BigDecimal("43213");
+		Portfolio expectedPortfolio = new Portfolio(1,"John Doe", "1001", portfolioValue, 23.4, "Safe");
+
+		when(portfolioRepository.findById(anyInt())).thenReturn(Optional.of(expectedPortfolio));
+		Portfolio actualPortfolio = portfolioService.getPortfolio(1);
+		// Assert
+		assertEquals(expectedPortfolio, actualPortfolio);
 	}
 
 }
