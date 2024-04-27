415a8a5192241a9c084624755fa977dbd736f4a5
Yumidu Jayarathna <Yumidu Jayarathna>
2024-04-27 14:57:19 +0530

    exception handling

---
+++ src/main/java/com/hcl/portfolio/exceptions/PositionNotFoundException.java
@@ -0,0 +1,13 @@
+package com.hcl.portfolio.exceptions;
+
+/**
+ *
+ * @author Yumidu Jayarathna
+ * @since 4/27/2024
+ */
+
+public class PositionNotFoundException extends RuntimeException{
+    public PositionNotFoundException(String message){
+        super(message);
+    }
+}
